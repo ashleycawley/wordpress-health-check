d0b6c3a551d32533adec9e36f215f717da23b092
Ashley Cawley <ash@ashleycawley.co.uk>
2018-08-09 13:18:27 +0100

    Moved WP-CLI Checksum test to function

--- wp-check.sh
+++ wp-check.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
 # A script to health-check and rate the integrity and health of a WordPress website
+
+# Test to see if the user is root (we don't want them to be root)
+if [ `whoami` == root ]
+then
+	echo && echo "DO NOT run this script as root. Please run this script as the user who owns the website." && echo
+	exit 1
+fi
+
+
+function CHECKSUMCORE {
+
+DLPATH=/tmp/
+
+# Downloads WP-CLI - http://wp-cli.org/
+curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --output ${DLPATH}wp-cli.phar
+
+# Checks to see if it is present and shows its version
+php ${DLPATH}wp-cli.phar --info
+
+# Checks the checksums of your WordPress website against what they should be
+php ${DLPATH}wp-cli.phar core verify-checksums
+
+# Removes WP-CLI from the system after work is done
+rm -f ${DLPATH}wp-cli.phar
+
+}
+
+read -p "Enter the full path of the WordPress site you wish to check: " SITEPATH
+
+cd $SITEPATH
+
+CHECKSUMCORE
