91e51ee44dc4791a992ceac8c5ad43a389de99fa
Ashley Cawley <ash@ashleycawley.co.uk>
2018-08-10 13:11:34 +0100

    Fixing if test, looking to see if the user is in the location of a WP site

--- wp-check.sh
+++ wp-check.sh
@@ -30,48 +30,52 @@ echo && read -p "Press [Enter] to proceed..." && echo
 
 }
 
-read -p "Enter the full path of the WordPress site you wish to check: " SITEPATH
-
-cd $SITEPATH
-
 if [ -f wp-config.php ]
 	then
 
-	# Downloads WP-CLI - http://wp-cli.org/
-	curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --output ${DLPATH}wp-cli.phar &>/dev/null
+SITEPATH=(`pwd`)
 
-	echo && echo "Website selected..."
-	php ${DLPATH}wp-cli.phar option list --search=siteurl
+else
 
-
-	echo && echo "Comparing the version of this WordPress website with the latest..." && sleep 2
-	php ${DLPATH}wp-cli.phar core check-update
-	PAUSEANDPROMPT
-
-	echo && echo -e "Checking number of installed Plugins and their status... \c"
-	php ${DLPATH}wp-cli.phar plugin status
-	PAUSEANDPROMPT
-
-	echo && echo -e "Checking installed Themes and their status... \c"
-	php ${DLPATH}wp-cli.phar theme status
-	PAUSEANDPROMPT
-
-	# echo && echo "Listing active Plugins..."
-	# php ${DLPATH}wp-cli.phar plugin list --status=active
-	# PAUSEANDPROMPT
-	
-	# echo && echo "Listing inactive Plugins..."
-	# php ${DLPATH}wp-cli.phar plugin list --status=inactive
-	# PAUSEANDPROMPT
-
-	echo "Testing the checksums of the WordPress Core files..."
-	CHECKSUMCORE
-	PAUSEANDPROMPT
-
-	echo "Listing Administrators..."
-	php ${DLPATH}wp-cli.phar user list --role=administrator
-	PAUSEANDPROMPT
+read -p "Enter the full path of the WordPress site you wish to check: " SITEPATH
 
 fi
+
+cd $SITEPATH
+
+# Downloads WP-CLI - http://wp-cli.org/
+curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --output ${DLPATH}wp-cli.phar &>/dev/null
+
+echo && echo "Website selected..."
+php ${DLPATH}wp-cli.phar option list --search=siteurl
+
+
+echo && echo "Comparing the version of this WordPress website with the latest..." && sleep 2
+php ${DLPATH}wp-cli.phar core check-update
+PAUSEANDPROMPT
+
+echo && echo -e "Checking number of installed Plugins and their status... \c"
+php ${DLPATH}wp-cli.phar plugin status
+PAUSEANDPROMPT
+
+echo && echo -e "Checking installed Themes and their status... \c"
+php ${DLPATH}wp-cli.phar theme status
+PAUSEANDPROMPT
+
+# echo && echo "Listing active Plugins..."
+# php ${DLPATH}wp-cli.phar plugin list --status=active
+# PAUSEANDPROMPT
+
+# echo && echo "Listing inactive Plugins..."
+# php ${DLPATH}wp-cli.phar plugin list --status=inactive
+# PAUSEANDPROMPT
+
+echo "Testing the checksums of the WordPress Core files..."
+CHECKSUMCORE
+PAUSEANDPROMPT
+
+echo "Listing Administrators..."
+php ${DLPATH}wp-cli.phar user list --role=administrator
+PAUSEANDPROMPT
 
 exit 0
